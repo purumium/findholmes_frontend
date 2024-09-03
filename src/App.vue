399e85d02fbdfbf5262d4f201048486a37de5bf8
pureum <purumium@gmail.com>
2024-09-03 23:44:27 +0900

    setting initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <HeaderCompo/>
+    <MiddleCompo/>
+    <FooterCompo/>
+  </div>
+</template>
+
+<script>
+import FooterCompo from './components/FooterCompo.vue';
+import HeaderCompo from './components/HeaderCompo.vue';
+import MiddleCompo from './components/MiddleCompo.vue';
+
+
+
+export default {
+  name: 'App',
+  components: {
+    HeaderCompo,
+    MiddleCompo,
+    FooterCompo
+  }
+}
+</script>
+
+<style>
+  @import 'assets/base.css';
+</style>
---
+++ src/components/User/ReceiveCompo.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>받은 견적 페이지</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
