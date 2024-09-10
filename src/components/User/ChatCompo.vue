2e3f2398c51fa68339e89830bcff282a0baefb7a
hyl27 <100427hyl@gmail.com>
2024-09-10 17:49:05 +0900

    websocket

--- src/components/User/ChatCompo.vue
+++ src/components/User/ChatCompo.vue
@@ -1,9 +1,12 @@
 <template>
   <div>채팅</div>
+  <div>
+    <button>채팅방 생성</button>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style scoped></style>
--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,20 @@
-const { defineConfig } = require('@vue/cli-service');
+const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
     port: 8000, // 개발 서버 포트 설정
     proxy: {
-      '/api': {
-        target: 'http://localhost:8080', // 백엔드 서버의 URL
+      "/api": {
+        target: "http://localhost:8080", // 백엔드 서버의 URL
         changeOrigin: true,
-        pathRewrite: { '^/api': '' }, // '/api'를 제거하여 실제 API 경로와 일치시킵니다.
+        pathRewrite: { "^/api": "" }, // '/api'를 제거하여 실제 API 경로와 일치시킵니다.
+      },
+      // 웹소켓 경로를 프록시 설정
+      "/ws": {
+        target: "ws://localhost:8080", // Spring Boot 서버의 웹소켓 주소
+        ws: true, // 웹소켓 프록시 활성화
+        changeOrigin: true,
       },
     },
   },
