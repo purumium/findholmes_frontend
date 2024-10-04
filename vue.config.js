98daab7fefad52de1696444a5fff14f842a39c62
ChoiYH96 <169019916+ChoiYH96@users.noreply.github.com>
2024-10-04 21:49:50 +0900

    .
    .

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,8 @@ module.exports = defineConfig({
     port: 8000, // 개발 서버 포트 설정
     proxy: {
       "/api": {
-        target: "http://localhost:8080", // 백엔드 서버의 URL
+        target:
+          "http://ec2-52-78-25-102.ap-northeast-2.compute.amazonaws.com:8080", // 백엔드 서버의 URL
         // target: "http://13.209.252.43",
         ws: false,
         changeOrigin: true,
