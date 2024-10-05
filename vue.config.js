9ca72d3450201d6b05be3d10d2900033fc19abc5
pureum <purumium@gmail.com>
2024-10-05 16:53:05 +0900

    vue config.js

--- vue.config.js
+++ vue.config.js
@@ -7,8 +7,9 @@ module.exports = defineConfig({
     proxy: {
       "/api": {
         target:
-          "http://ec2-52-78-25-102.ap-northeast-2.compute.amazonaws.com:8080", // 백엔드 서버의 URL
-        // target: "http://13.209.252.43",
+          // "http://ec2-52-78-25-102.ap-northeast-2.compute.amazonaws.com:8080", // 백엔드 서버의 URL
+          // target: "http://13.209.252.43",
+          "http://localhost:8080",
         ws: false,
         changeOrigin: true,
         pathRewrite: { "^/api": "" }, // '/api'를 제거하여 실제 API 경로와 일치시킵니다.
